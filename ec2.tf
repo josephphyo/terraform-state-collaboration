71fa31779e688746faeaa8dd8555139a13d83597
Ye Min <yemin.phyo@ascendcorp.com>
2020-11-17 22:39:02 +0630

    fix: merge main

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webserver" {
   instance_type = var.ec2.instance_type
   ami           = var.ec2.image_id
   key_name      = var.ec2.key_name
-  subnet_id = module.vpc.public_subnets[0]
+  subnet_id     = module.vpc.public_subnets[0]
 
   tags = {
     Name = var.ec2.name
