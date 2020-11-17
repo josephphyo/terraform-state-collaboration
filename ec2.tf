0ef69c961b86ea94a77d7c254df87b9fa71f8ded
Phyo Min Htun <phyominhtun@Phyos-MacBook-Pro-2.local>
2020-11-17 21:17:05 +0630

    fix: variable and vpc

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "webserver" {
   instance_type          = var.instance_type
   ami                    = var.image_id
   key_name               = var.key_name
+  subnet_id		 = var.public_subnets
 
   tags = {
     Name = "TF_Server_Ubuntu"
--- variable.tf
+++ variable.tf
@@ -21,20 +21,11 @@ variable "vpc" {
   description = "ALL IN ONE VPC VARIABLES"
 }
 
-variable "instance_type" {
-  default     = "t3.micro"
-  type        = string
-  description = "AWS Instance Size"
-}
-
-variable "image_id" {
-  default     = "ami-093da183b859d5a4b"
-  type        = string
-  description = "AMI for Ubuntu 18.04"
-}
-
-variable "key_name" {
-  default     = "k8smm"
-  type        = string
-  description = "SSH Pub Key"
+variable "ec2" = {
+  type = object({
+    instance_type = string
+    image_id = string 
+    key_name = string 
+  })
+  description = "ALL IN ONE EC2 VARIABLE"
 }
