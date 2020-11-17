d6b8e2086a51555db88fa79622c9c769ed1ce217
Wai Yan Min <waiyanmin@yomafleet.com>
2020-11-17 20:55:05 +0630

    fix: remove old files

--- variable.tf
+++ variable.tf
@@ -13,7 +13,6 @@ variable "vpc" {
     private_subnets         = list(string)
     database_subnets        = list(string)
     vpc_id                  = string
-    cache_subnet_group_name = string
     env                     = string
     owner                   = string
     project                 = string
@@ -21,26 +20,6 @@ variable "vpc" {
   })
   description = "ALL IN ONE VPC VARIABLES"
 }
-
-#variable "ec2" {
-#  type = object({
-#    name_api           = string
-#    name_admin         = string
-#    api_eip_alloc_id   = string
-#    admin_eip_alloc_id = string
-#    ami_id             = string
-#    is_monitor         = bool
-#    ins_type           = string
-#    ins_count          = number
-#    keyname            = string
-#    ebs_optimize       = bool
-#    iam_profile        = string
-#    volume_tags        = map(string)
-#   ssh_sg             = string
-
-#  })
-#  description = "ALL IN ONE EC2 VARIABLES"
-#}
 
 variable "instance_type" {
   default     = "t3.micro"
